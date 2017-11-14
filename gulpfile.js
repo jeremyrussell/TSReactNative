63ee272bf3ce0cdb628268ef57a54d47fae9fb13
Jeremy Russell <att.jeremyr@sbmelmac-06714.local>
2017-11-15 08:53:33 +1100

    finished exercise 10

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
+var del = require('del');
+var ts = require('gulp-typescript');
+
 
 gulp.task('default', function() {
   return gulp.src('src/web/App.js')
     .pipe( webpack(require('./webpack.config.js')) )
     .pipe(gulp.dest('./artifacts/web'));
 });
+
+gulp.task('clean-native', function() {
+    return del('lib');
+});
+
+// gulp.task('compile-native', function() {
+//     var tsProject = ts.createProject('tsconfig.json');
+//     return tsProject.src() // or tsProject.src()
+//         .pipe(tsProject());
+// });
+
